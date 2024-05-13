e733b0c90a3aa5b2cbd4de6bad69a1b79322a51d
Dvir <rabbanidvir@gmail.com>
2024-05-13 22:03:51 +0300

    wip - use user service(httpService)

--- frontend/src/services/user/index.js
+++ frontend/src/services/user/index.js
@@ -1,5 +1,5 @@
-// import { _userService } from "./user.service"
-import { _userService } from "./user.service.local"
+import { _userService } from "./user.service"
+// import { _userService } from "./user.service.local"
 
 const userService = _userService
 export default userService
