a6bb557d9edf64a486e9a1719d6ad5adc1715540
Dvir <rabbanidvir@gmail.com>
2024-05-16 15:38:06 +0300

    wip - bug preview - add bug owner link

--- frontend/src/cmps/bug/BugPreview.jsx
+++ frontend/src/cmps/bug/BugPreview.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom"
+
 export function BugPreview({ bug }) {
   return (
     <article>
       <h4>{bug.title}</h4>
       <h1>🐛</h1>
+      {bug.owner && (
+        <h4>
+          Owner: <Link to={`/user/${bug.owner._id}`}>{bug.owner.fullname}</Link>{" "}
+        </h4>
+      )}
       <p>
         Severity: <span>{bug.severity}</span>
       </p>
