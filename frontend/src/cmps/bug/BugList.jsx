33ecb8cc04cedf8df4c50d7214ff6b1cce9539b8
Dvir <rabbanidvir@gmail.com>
2024-05-16 16:45:26 +0300

    wip - bug list - handle when to display actions button

--- frontend/src/cmps/bug/BugList.jsx
+++ frontend/src/cmps/bug/BugList.jsx
@@ -1,28 +1,37 @@
 import { Link } from "react-router-dom"
 import { BugPreview } from "./BugPreview"
+import userService from "../../services/user"
 
 export function BugList({ bugs, onRemoveBug, onEditBug }) {
+  function shouldShowActionBtns(bug) {
+    const user = userService.getLoggedinUser()
+    if (!user) return false
+    if (user.isAdmin) return true
+    return bug.owner?._id === user._id
+  }
   return (
     <ul className="bug-list">
       {bugs.map((bug) => (
         <li className="bug-preview" key={bug._id}>
           <BugPreview bug={bug} />
-          <div>
-            <button
-              onClick={() => {
-                onRemoveBug(bug._id)
-              }}
-            >
-              x
-            </button>
-            <button
-              onClick={() => {
-                onEditBug(bug)
-              }}
-            >
-              Edit
-            </button>
-          </div>
+          {shouldShowActionBtns && (
+            <section className="bug-btn-actions">
+              <button
+                onClick={() => {
+                  onRemoveBug(bug._id)
+                }}
+              >
+                x
+              </button>
+              <button
+                onClick={() => {
+                  onEditBug(bug)
+                }}
+              >
+                Edit
+              </button>
+            </section>
+          )}
           <Link to={`/bug/${bug._id}`}>Details</Link>
         </li>
       ))}
