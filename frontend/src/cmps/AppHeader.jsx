9e4521085fb3bd15223dd53498b6cafbf7f9c2f8
Dvir <rabbanidvir@gmail.com>
2024-05-16 16:43:42 +0300

    wip - frontend app header - navigate to user page when user is admin

--- frontend/src/cmps/AppHeader.jsx
+++ frontend/src/cmps/AppHeader.jsx
@@ -26,7 +26,8 @@ export function AppHeader() {
       <div className="header-container">
         <nav className="app-nav">
           <NavLink to="/">Home</NavLink> |<NavLink to="/bug">Bugs</NavLink> |
-          <NavLink to="/about">About</NavLink>
+          <NavLink to="/about">About</NavLink> |
+          {user?.isAdmin && <NavLink to="/user">Users</NavLink>}
         </nav>
         <h1>Bugs are Forever</h1>
       </div>
